359d64a2e3570d245b0e4b162cb7ca28da025242
Jordan SABLON <jordansablon.enseignement@gmail.com>
2026-04-03 16:21:18 +0200

    GEstion du CSS du filtre actif

--- frontend/src/components/Filters/Filters.tsx
+++ frontend/src/components/Filters/Filters.tsx
@@ -1,14 +1,23 @@
+import { useState } from "react";
+
 type FiltersProps = {
     onFilter: (done?: boolean) => void
 }
 
 export const Filters = ({ onFilter }: FiltersProps) => {
 
+    const [selectedFilter, setSelectedFilter] = useState<number>(0);
+
+    const filter = (done: boolean | null) => {
+        onFilter(done === null ? undefined : done);
+        setSelectedFilter(done === null ? 0 : done ? 2 : 1)
+    }
+
     return (
         <div className="filters">
-            <button className="active" onClick={() => onFilter()}>Toutes</button>
-            <button onClick={() => onFilter(false)}>A faire</button>
-            <button onClick={() => onFilter(true)}>Terminées</button>
+            <button className={selectedFilter === 0 ? "active" : ""} onClick={() => filter(null)}>Toutes</button>
+            <button className={selectedFilter === 1 ? "active" : ""} onClick={() => filter(false)}>A faire</button>
+            <button className={selectedFilter === 2 ? "active" : ""} onClick={() => filter(true)}>Terminées</button>
         </div>
     )
 }
